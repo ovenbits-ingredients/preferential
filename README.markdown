a2a1d2373c5716cc3dd59adcb39d70cebd8ce746
Scotty Moon <scotty@scottymoon.com>
2011-12-05 22:01:50 -0600

    initial commit

--- README.markdown
+++ README.markdown
@@ -1,8 +1,19 @@
-= preferential
+# Preferential
 
-Description goes here.
+Preferential helps take the tedium out of adding preferences to your rails application.
 
-== Contributing to preferential
+## usage
+
+    Class User
+      include Preferential
+      pref_accessible :receive_account_email
+    end
+    
+    u = User.new
+    u.receive_account_email = true
+    u.save
+
+## Contributing to preferential
  
 * Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet
 * Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it
@@ -12,7 +23,7 @@ Description goes here.
 * Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
 * Please try not to mess with the Rakefile, version, or history. If you want to have your own version, or is otherwise necessary, that is fine, but please isolate to its own commit so I can cherry-pick around it.
 
-== Copyright
+## Copyright
 
 Copyright (c) 2011 Scotty Moon. See LICENSE.txt for
 further details.
---
+++ lib/preferential.rb
@@ -0,0 +1,47 @@
+module Preferential
+
+  def self.included(base)
+    base.extend(ClassMethods)
+
+    if base.respond_to?(:serialize)
+      base.serialize "#{base.name.downcase}_preferences".to_sym
+    end
+
+    if base.respond_to?(:after_find)
+      base.after_find :load_preferences
+    end
+
+  end
+
+  module ClassMethods
+    def pref_accessible(*args)
+      define_method(:preferences){args}
+      args.each do |method|
+        define_method(method){get_preference(method)}
+        define_method("#{method}=".to_sym) do |value|
+          set_preference(method,value)
+        end
+      end
+    end
+  end
+
+  def load_preferences
+    # clean up user_preference hash
+    # • initialize to empty hash if blank?
+    # • remove orphaned key/value pairs
+    class_preferences = "self.#{self.class.name.downcase}_preferences"
+    eval("#{class_preferences} = {}") if eval(class_preferences).blank?
+    eval(class_preferences).keep_if{|k,v| preferences.include?(k)}
+  end
+
+  def get_preference(pref_name)
+    class_preferences = eval("self.#{self.class.name.downcase}_preferences")
+    class_preferences[pref_name]
+  end
+
+  def set_preference(pref_name, pref_value)
+    class_preferences = eval("self.#{self.class.name.downcase}_preferences")
+    class_preferences[pref_name] = pref_value
+  end
+
+end
